815bd1b34aebe463a51ea3a081ff9d97101a45f5
nemogoc <nem_a_toad@hotmail.com>
2018-09-28 12:50:33 -0500

    implement get/create League

--- handlers/leagues.js
+++ handlers/leagues.js
@@ -1,7 +1,5 @@
 const db = require("../models");
 
-// getLeagues, getLeague, createLeague, deleteLeague
-
 exports.getLeagues = async function(req, res, next) {
     try {
         let leagues = await db.League.find().sort({createdAt: "desc"});
@@ -12,10 +10,33 @@ exports.getLeagues = async function(req, res, next) {
     }
 }
 
+exports.getLeague = async function(req, res, next) {
+    try {
+        let league = await db.League.findById(req.params.id); //TODO: messageId?
+        return res.status(200).json(league);
+    }
+    catch(err) {
+        return next(err);
+    }
+}
+
+exports.createLeague = async function(req, res, next) {
+    const {year, season, weekday} = req.params;
+    try {
+        let league = await db.League.create({
+            year,
+            season,
+            weekday,
+            teams: [],
+            locations: []
+        });
+
+        return res.status(200).json(league);
+    }
+    catch(err) {
+        return next(err);
+    }
+}
 
 //TODO: implement below
-exports.getLeague = async function(req, res, next) {}
-
-exports.createLeague = async function(req, res, next) {}
-
 exports.deleteLeague = async function(req, res, next) {}
