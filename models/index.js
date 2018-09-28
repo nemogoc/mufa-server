6bd224cf0cec0e5780426d9b171f39e9c865086f
nemogoc <nem_a_toad@hotmail.com>
2018-09-28 12:49:57 -0500

    update db loc

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //TODO: parameterize debug and mongo URL
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost:27017/warbler", {
+mongoose.connect("mongodb://localhost:27017/mufa", {
   keepAlive: true,
 });
 
